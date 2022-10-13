76ea4d63d96d73460526ad7588d4ba29899f9472
tonio-py <antonioyeboaux@gmail.com>
2022-10-13 06:26:21 -0700

    thisis santonehr message of teh cross

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	printf("Hello, World");
-	return (0);
+
+		printf("Holberton School");
+
+			return (0);
+
 }
+
+
+
+
+
+
