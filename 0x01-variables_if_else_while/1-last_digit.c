9e4b62828e55094fa25f0ea797031fc7bedc6ad1
tonio-py <antonioyeboaux@gmail.com>
2022-10-14 16:48:43 -0700

    this is another one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,15 @@ int main(void)
 	len = strlen(text);
 	digit = (text[len - 1]) - '0';
 
+	if (n < 0)
+	{
+		digit = -digit;
+	}
 	if (digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (digit < 0)
+	else if (digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
