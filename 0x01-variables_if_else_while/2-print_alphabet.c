a564c9c6ebcb197d3094ad942debbeb29ff01517
tonio-py <antonioyeboaux@gmail.com>
2022-10-14 17:03:28 -0700

    this is now understanding

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x < 123)
+	{
+		putchar((char) x);
+		putchar('\n');
+		x++;
+	}
+	return (0);
+}
