29025a52e9268aaca622a482a455229989ad7c32
tonio-py <antonioyeboaux@gmail.com>
2022-10-14 17:41:04 -0700

    thisis yours

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
