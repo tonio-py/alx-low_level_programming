eed492d6b60e64e51f2afd82ccb08464ee86d182
tonio-py <antonioyeboaux@gmail.com>
2022-10-18 22:55:36 -0700

    this is all

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: takes integer y
+ * @y: takes integer x
+ * Return: sum of a and b
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+
+}
