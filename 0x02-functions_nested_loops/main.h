ebf9991fb0ab8dbe38549b8e79da38dcef297deb
tonio-py <antonioyeboaux@gmail.com>
2022-10-18 20:18:13 -0700

    this is the message

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_H
+#define main_H
+
+int _putchar(char);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+
+#endif
