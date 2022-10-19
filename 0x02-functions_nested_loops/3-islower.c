bfd5df065799f6299031510e6f398ed3b0bed970
tonio-py <antonioyeboaux@gmail.com>
2022-10-18 21:30:21 -0700

    i am almost done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower- used to check if the letter is lowercase or not`
+ * @c: takes integer input
+ *
+ * Return: 1 if success and 0 if fail
+ */
+int _islower(int c)
+{
+	if (c > 96)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
