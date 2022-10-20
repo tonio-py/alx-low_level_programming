db10dd0863fa31fae8a4156b6954bf8dcc953e84
tonio-py <antonioyeboaux@gmail.com>
2022-10-20 15:59:31 -0700

    thisi sis another one

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 through 14 10 times
+ *
+ * Return: returns nothing
+ */
+
+void more_numbers(void)
+{
+	int x = 0;
+	int y = 0;
+
+	while (y < 10)
+	{
+		while (x < 15)
+		{
+			if (x > 9)
+			{
+				_putchar(x / 10 + '0');
+			}
+			_putchar(x % 10 + '0');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+		x = 0;
+	}
+	_putchar('\n');
+}
