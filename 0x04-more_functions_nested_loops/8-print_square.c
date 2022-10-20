1a0a4251f237da817506b2d9ce7020dd2b336d21
tonio-py <antonioyeboaux@gmail.com>
2022-10-20 16:36:50 -0700

    this is another one of them and i i am not really sure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square in the terminal
+ * @size: represents the lenght of the square
+ * Return: returns nothing
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	x = size;
+	y = size;
+
+	while (x > 0)
+	{
+		while (y > 0)
+		{
+			_putchar('#');
+			y--;
+		}
+		_putchar('\n');
+		y = size;
+		x--;
+	}
+}
