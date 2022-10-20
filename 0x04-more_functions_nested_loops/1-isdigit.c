0255e63759ed235bb3a6c7c632c4f15f9766b443
tonio-py <antonioyeboaux@gmail.com>
2022-10-20 14:55:37 -0700

    this is me

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if argument is a digit
+ * @c: takes c as an integer
+ * Return: returns 1 if it's a digit  and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9'){
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
