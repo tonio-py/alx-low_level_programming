e5c9aef36b295b64bf1fdbd98498fd941cd34c80
tonio-py <antonioyeboaux@gmail.com>
2022-10-20 15:04:34 -0700

    this is really interestng

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - solves for the product of two numbers
+ * @a: takes a as an integer
+ * @b: takes b as an integer
+ * Return: returns the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
