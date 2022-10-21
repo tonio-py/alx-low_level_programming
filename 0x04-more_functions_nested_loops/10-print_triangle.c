74062bfab779e510b2f93bc2683bdd52cf60c09d
tonio-py <antonioyeboaux@gmail.com>
2022-10-20 17:08:29 -0700

    this is you

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: takes the size of the triangle
+ * Return: returns nothing
+ */
+void print_triangle(int size)
+{
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
